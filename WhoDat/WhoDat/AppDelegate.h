30e172c2117eefddcbe7c4a20d7f123c4f176bcd
Anthony Williams <anthony.williams@student.csulb.edu>
2015-02-21 17:33:53 -0800

    PLEASEEE

---
+++ WhoDat/WhoDat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WhoDat
+//
+//  Created by Anthony Williams on 2/21/15.
+//  Copyright (c) 2015 Anthony Williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
